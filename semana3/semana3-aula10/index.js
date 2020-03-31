7413ff75e71bbe4ae879eb6fb4956e28ecfd3dd4
axpalx <axpalx@gmail.com>
2020-03-31 16:28:30 -0300

    Exercicios da tarde aula 10

---
+++ semana3/semana3-aula10/index.js
@@ -0,0 +1,128 @@
+// Exercicio 1
+// const respostaDoUsuario = prompt("Digite o número que você quer testar?")
+// 	Resposta: recebe constatnte do usuário
+
+// const numero = Number(respostaDoUsuario)
+// 	Resposta: Declara a constante numero e recebe o valor transformado em numero da constante respostaDoUsuario
+
+// if(numero % 2 === 0) {
+// 		Resposta: Testa se o resultado do MOD do número digitado é estritamento igual a 0.
+//   console.log("Passou no teste.")
+// 		Resposta: Se for igual exibe a mensagem que "Passou no teste"
+// } else {
+// 		console.log("Não passou no teste.")
+//   Resposta: Senão foi reprovado
+// }
+
+// Exercicio 2
+
+// let fruta = prompt("Escolha uma fruta")
+// let preco
+// switch (fruta) {
+//   case "Laranja":
+//     preco = 3.5
+//     break;
+//   case "Maçã":
+//     preco = 2.25
+//     break;
+//   case "Uva":
+//     preco = 0.30
+//     break;
+//   case "Pêra":
+//     preco = 5.5
+//     break; // BREAK PARA O ITEM d.
+//   default:
+//     preco = 5
+//     break;
+// }
+// console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
+// Respostas:
+// 			A - Exiber o preço da fruta selecionado pelo usuário.
+
+// 			B - "O preço da fruta Maçã é R$ 2.25"
+
+// 			C - O código desenvolvido não faz cálculo
+
+// 			D - Ele entende que o SWITCH não encontrou a escolha e continua a execução
+
+// Exercicio 3
+
+// Resposta: Sim haverá erro, pois o consolo.log não irá reconhecer a variável mensagem, pois ela está dentro do bloco if, ou seja fora do escopo.
+
+// Exercicio 4
+
+// Ítem A
+
+// numA = Number(prompt("Digite o valor de A"))
+// numB = Number(prompt("Digite o valor de B"))
+// temp = 0
+// if(numB>numA){
+//     temp=numB
+//     numB=numA
+//     numA=temp
+//     console.log("Ordem decrescente "+numA+", "+numB)
+// } else {
+//      console.log("Ordem decrescente "+numA+", "+numB)
+// }
+// Comentário: O códigoquebra.
+
+// Ítem B
+
+// numA = Number(prompt("Digite o valor de A"))
+// numB = Number(prompt("Digite o valor de B"))
+// numC = Number(prompt("Digite o valor de C"))
+// temp = 0
+
+// if(numB > numA){
+//     temp=numB
+//     numB=numA
+//     numA=temp
+// }
+// if(numC > numA){
+//     temp=numC
+//     numC=numA
+//     numA=temp
+// }
+// if(numC > numB){
+//     temp=numC
+//     numC=numB
+//     numB=temp
+//     console.log("Ordem decrescente "+numA+" - "+numB+" - "+numC)
+// }
+//O código quebrou, não trouxe resultado
+
+// Ítem C
+
+// numA = Number(prompt("Digite o valor de A"))
+// numB = Number(prompt("Digite o valor de B"))
+// numC = Number(prompt("Digite o valor de C"))
+// temp = 0
+// if(numA===numB && numA===numC && numB===numC) {
+//     console.log("Os números não podem ser iguais, você deve inserir números diferentes")
+// } else {
+//     if(numB > numA){
+//         temp=numB
+//         numB=numA
+//         numA=temp
+//     }
+//      if(numC > numA){
+//         temp=numC
+//         numC=numA
+//         numA=temp
+//     }
+//     if(numC > numB){
+//         temp=numC
+//         numC=numB
+//         numB=temp
+//         console.log("Ordem decrescente "+numA+" - "+numB+" - "+numC)
+//     }    
+// }
+
+// Exercicio 5
+
+// Ítem A
+
+// https://drive.google.com/open?id=1L6_yQ0Em5Qzr37Mc9mTHOrzs1lMql593
+
+
+    
