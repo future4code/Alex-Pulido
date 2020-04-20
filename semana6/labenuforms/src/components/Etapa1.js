fc9db7691ffc8b1437ce98720a20ab7c53ffbdf0
axpalx <axpalx@gmail.com>
2020-04-20 17:31:56 -0300

    labenuforms-alex-pulido.surge.sh

---
+++ semana6/labenuforms/src/components/Etapa1.js
@@ -0,0 +1,38 @@
+// import React from 'react';
+
+
+
+// export class Etapa1 extends React.Component {
+//     vstate = {
+//         nomeConcorrente: "",
+//         idadeConcorrente: "",
+//         emailsConcorrente: "",
+//         escolaridadeConcorrente: ""
+//     };
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>ETAPA 1 - DADOS GERAIS</h1>
+//                 <h2>1. Qual o seu nome?</h2>
+//                 <input placeholder="Nome"></input>
+//                 <h2>2. Qual a sua idade?</h2>
+//                 <input placeholder="Idade"></input>
+//                 <h2>3. Qual seu e-mail?</h2>
+//                 <input placeholder="E-mail"></input>
+//                 <select>
+//                     <option value="ensinoMedioIncompleto">Ensino Médio Incompleto</option>
+//                     <option value="ensinoMedioCompleto">Ensino Médio Completo</option>
+//                     <option value="ensinoSuperiorIncompleto">Ensino Superior Incompleto</option>
+//                     <option value="ensinoSuperiorIncompleto">Ensino Superior Incompleto</option>
+//                 </select>
+//             </div>
+//         )
+//     }
+
+
+
+
+
+// }
+
