4a9a91a38e5c9f9132a1fa9884c6dfb5ea6237c8
axpalx <axpalx@gmail.com>
2020-04-24 19:28:27 -0300

    Restruturacao de pastas

---
+++ semana6/Aula-22/boiler-plate/build/precache-manifest.d8f706dabf097dc6a0afdcb95df42205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625d5e10bf24ea74b1bd2a9dd2517e63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d073ce02529ef4d1c93",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "0bc709225786e34ebc3e",
+    "url": "/static/js/2.ce6ba238.chunk.js"
+  },
+  {
+    "revision": "1d073ce02529ef4d1c93",
+    "url": "/static/js/main.db81eff4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
